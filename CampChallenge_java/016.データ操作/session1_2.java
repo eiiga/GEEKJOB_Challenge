a42edca8331fcae97a07547e36b0ab3cb38402a4
LAPTOP-UBIOPS8M\ei-tk <ei.tktkwk.ud.f.603@gmai.com>
2017-09-21 15:58:11 +0900

    データ操作の課題、セッション1を提出します。ご確認よろしくお願いします。

---
+++ CampChallenge_java/016.データ操作/session1_2.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author ei-tk
+ */
+public class session1_2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            HttpSession session = request.getSession();
+            long createTime = session.getCreationTime();
+            Date createDate = new Date(createTime);
+            long accessTime = session.getLastAccessedTime();
+            Date accessDate = new Date(accessTime);
+                out.println("セッション開始時間:" + createDate + "<br>");
+                out.println("最終アクセス時間  :" + accessDate);
+              }
+    }     
+              
+             
+            /* TODO output your page here. You may use following sample code. */
+//            HttpSession[] session = request.getSession();
+//            HttpSession accesstimecookie = null;
+//            if(session != null){
+//                for(int i = 0;i < session.length;i++){
+//                    if(session[i].getName().equals("accesstime")){
+//                        accesstimecookie = session[i];
+//                    }
+//                }
+//            }
+//            String accesstime;
+//            if(accesstimecookie == null){
+//                accesstime = "記録がありません。";
+//            }else{
+//                accesstime = URLDecoder.decode(accesstimecookie.getValue());
+//            }
+//            out.print(accesstime);
+        
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
