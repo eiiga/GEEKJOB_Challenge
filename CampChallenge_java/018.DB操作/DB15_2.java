80f312c960cd2ad2859ab65f4f4b7030984dbe50
eiiga <31603417+eiiga@users.noreply.github.com>
2019-05-04 23:06:08 +0900

    Update DB15_2.java

--- CampChallenge_java/018.DB操作/DB15_2.java
+++ CampChallenge_java/018.DB操作/DB15_2.java
@@ -46,7 +46,7 @@ public class DB15_2 extends HttpServlet {
             
             try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
-            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db","eiigarashi","igarashiei0563");
+            db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db","***","***");
             
             db_st = db_con.prepareStatement("select * from profiles where name = ? and age = ? and birthday = ?");
             db_st.setString(1, name);
